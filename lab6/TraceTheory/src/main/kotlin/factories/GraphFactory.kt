47110b504c29e34233e37c78b9bc38abd60647e9
Filip Dziurdzia <filipdziurdzia2002@gmail.com>
2023-11-20 22:18:58 +0100

    added better documentation

--- lab6/TraceTheory/src/main/kotlin/factories/GraphFactory.kt
+++ lab6/TraceTheory/src/main/kotlin/factories/GraphFactory.kt
@@ -16,6 +16,13 @@ class GraphFactory {
 
     companion object {
 
+        /***
+         * Method for generating visualization of a graph. Uses Graphviz with Kotlin API. You need to install Graphviz to generate graphs with this program.
+         * Docs can be found here: https://github.com/nidi3/graphviz-java#kotlin-dsl
+         * @param graph Graph
+         * @param path Name or path where the generated graph should be saved.
+         * @return Visualized graph as a png saved in the graphs directory + provided path.
+         */
         fun printGraph(g: Graph, path: String) {
             graph(directed = true) {
                 edge["color" eq "black", Arrow.VEE]
